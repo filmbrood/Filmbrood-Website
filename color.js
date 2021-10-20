c8c44b628d42458688fcc6785f965f075ff17f4b
filmbrood <filmbrood@gmail.com>
2021-10-20 00:50:56 -0500

    Refactored navlinks in color.html into JavaScript code Experiment for future endeavours ;D

---
+++ color.js
@@ -0,0 +1,21 @@
+const createNavLinkElement = (href, innerHTML, style) => {
+  let navLinkItemElement = document.createElement("a");
+  navLinkItemElement.classList.add(style);
+  navLinkItemElement.href = href;
+  navLinkItemElement.innerHTML = innerHTML;
+
+  return navLinkItemElement;
+}
+
+let navLinkItems = [
+  createNavLinkElement("index.html", "Cinematography", "navlink-item"),
+  createNavLinkElement("color.html", "Color Grading", "navlink-item-active"),
+  createNavLinkElement("about.html", "About", "navlink-item")
+];
+
+let navlinkDivElem = document.getElementById("navlinks");
+
+for (let i = 0; i < 3; i++)
+{
+  navlinkDivElem.appendChild(navLinkItems[i]);
+}
