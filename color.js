f0dddfd84a849fd8f83b9c33a68eb163f0e99e71
filmbrood <filmbrood@gmail.com>
2021-10-21 20:19:10 -0500

    Converted Color Grading Page to JavaScript

--- color.js
+++ color.js
@@ -1,5 +1,32 @@
-import { FWAPI } from "./fwapi.js";
+import { FWAPI } from "./fwapi.js"
+import { CinemaPortfolioItem, EmbeddedVimeo } from "./fwapi.js";
 
-console.log("color.js loaded");
+console.log("index.js loaded");
+
 FWAPI.ActivePageInt = 1;
-FWAPI.AppendNavlinks("navlinks");
+
+let navLinksContainer = FWAPI.CreateElement("div", "", "div");
+let navLinks = FWAPI.CreateElement("div", "", "navlinks");
+
+FWAPI.AppendNavlinks(navLinks);
+FWAPI.AppendElement(navLinksContainer, navLinks);
+FWAPI.AppendElement("page", navLinksContainer);
+
+const portfolioPage = FWAPI.CreateElement("div", "", "portfolio-page");
+FWAPI.AppendElement("page", portfolioPage);
+
+let cinematographyReel = new EmbeddedVimeo("https://player.vimeo.com/video/577076137?h=b6fd4598b1");
+cinematographyReel.AppendTo(portfolioPage);
+
+//const cinemaPortfolioHeader = FWAPI.CreateElement("h1", "PORTFOLIO", "cinema-portfolio-header");
+const cinemaPortfolio = FWAPI.CreateElement("div", "", "cinema-portfolio");
+
+/* FWAPI.AppendElement(portfolioPage, cinemaPortfolioHeader);
+
+let portfolioItems = [];
+
+for (let i = 0; i < portfolioItems.length; i++)
+  FWAPI.AppendElement(cinemaPortfolio, portfolioItems[i].Get());
+
+FWAPI.AppendElement(portfolioPage, cinemaPortfolio);
+*/
